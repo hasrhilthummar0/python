44984f1518131ab250abdb19daf5d6d7dfa5c748
hasrhilthummar0 <134331438+hasrhilthummar0@users.noreply.github.com>
2023-05-23 01:47:38 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+# a = "1"
+# b = "2"
+# print(int(a)+int(b))
+
+# string = "15"
+# q1 = 7
+# string_q1= int(string)
+# sum=q1+string_q1
+# print("the both of sum is",sum)
+
+
+#                     implicit type casting
+
+a =10.9
+b=1
+print("the both sum is ",a+b)
+
+
---
+++ userinput.py
@@ -0,0 +1,11 @@
+# a = input("enter your name :")
+# print("this is my name",a)
+
+
+# x= input("enter your first number :")
+# y= input("enter your scind number :")
+# print("the both number sum is ",int(x) +int(y))
+
+x= input("enter your first numbr :")
+y= input("enter your second number :")
+print(int(x) + int(y))
