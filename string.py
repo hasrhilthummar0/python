12e33dd331c322958f861cd9e4d15ee27aab3414
hasrhilthummar0 <134331438+hasrhilthummar0@users.noreply.github.com>
2023-05-23 02:03:58 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+# peom='''
+# hello my name is harshil
+# and my surname is thummar
+# and now i am study in bca in charusat univecity
+# '''
+# print(peom)
+
+name="harshil"
+friend="manav"
+otherfriend="sneh"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+
+# print(name[8]) thorow index error
+print("let we use for loop in string\n")
+
+#     lest use for loop in string
+
+for character in name :
+    print(character)
