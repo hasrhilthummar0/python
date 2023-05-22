293f55a5105b788b1eace6ea6b847340051d8196
hasrhilthummar0 <134331438+hasrhilthummar0@users.noreply.github.com>
2023-05-23 02:21:55 +0530

    Add files via upload

---
+++ string slicing  & operations on string.py
@@ -0,0 +1,17 @@
+# name="harshil,thummar"
+# print(name[0:7])
+#
+# name="harshil,thummar"
+# print(len(name))
+
+
+# fruit="mango"
+# len1=len(fruit)
+ # print("the alphabet in mango is",len1,"are include in that")
+
+fruit="mango"
+len1=len(fruit)
+print(fruit[0:4]) #it print 3 digit in length
+print(fruit[1:4]) #it take start with number 1 digit
+print(fruit[:5]) # it mean they print all character
+print(fruit[0 :-3]) # it mean where have -3 it take len(fruit)-3 remember digit print
